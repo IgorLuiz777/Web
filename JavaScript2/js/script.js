7ba4a2a0d5cffcce04d0e32c85ec5b7e1a2c650f
Igorritos <79776778+Igorritos@users.noreply.github.com>
2023-05-10 08:53:52 -0500

    rere

---
+++ JavaScript2/js/script.js
@@ -0,0 +1,117 @@
+
+
+// let aluno = confirm('Você já é nosso aluno?')
+
+// console.log(aluno ? 'Bons estudos!!' : 'Você não quer se matricular?')
+
+//let nome = prompt('Digite seu nome completo:')
+
+// console.log(nome)
+// console.log('Seja bem vindo '+ nome.slice(0, nome.indexOf(' '))+'!!')
+
+// let num1 = Number(prompt('Digite o primeiro número:'))
+// let num2 = Number(prompt('Digite o segundo número: '))
+
+// console.log(num1 + num2)
+
+// let aluno = true
+
+// if(aluno = true) console.log('Bons estudos!'); else console.log('Você não quer se matricular?')
+
+// let idade = 16
+
+// if(idade < 16) {
+//     console.log('Você ainda não pode votar!');
+// }else if(idade < 18){
+//     console.log('Você pode votar se quiser!')
+// }else{
+//     console.log('Você deve votar!')
+// }
+
+// // && = and / || = or
+
+// if(aluno && idade >= 16){
+//     console.log('Você pode participar!')
+// }else{
+//     console.log('Você não pode participar')
+// }
+
+// let num1 = '12'
+
+// if(num1 == 12){ // valores iguais
+//     console.log('São iguais');
+// }else{
+//     console.log('São diferentes')
+// }
+
+
+// if(num1 === 12){ // valores e tipos iguais
+//     console.log('São iguais');
+// }else{
+//     console.log('São diferentes')
+// }
+
+
+// let opcao = 2
+
+// switch (opcao) {
+//     case 1:
+//         console.log('Ele escolheu a opção 1');
+//         break;
+//     case 2:
+//         console.log('Ele escolheu a opção 2');
+//         break;
+//     case 3:
+//         console.log('Ele escolheu a opção 3');
+//         break;
+//     default:
+//         console.log('Ele escolheu uma opção inválida')
+//         break;
+// }
+
+// let carros = ['Civic', 'Corolla', 'Sentra', 'Cruze', 'Cronos']
+
+// for(let i = 0; i < carros.length; i++){ // default
+//     console.log(carros[i]);
+// }
+
+// for(let carro in carros){ // retorna a posição no array
+//     console.log(carro)
+// }
+
+// for(let carro of carros){ // retorn a posição no array
+//     console.log(carro)
+// }
+
+// carros.forEach(function(carro, i){
+//     console.log(`O carro ${carro} está na posição ${i+1}°`)
+// })
+
+// let cont = 0 
+
+// while (cont < 5){
+//     console.log(cont);
+//     cont++
+// }
+
+// do {
+//     console.log(cont)
+//     cont++
+// }while(cont < 5)
+
+// function aviso(){
+//     alert('Testando a função aviso!')
+// }
+
+// function somar(arg1, arg2){
+//     return arg1 + arg2
+// }
+
+// console.log(somar(7,5))
+
+// let aviso2 = function(){
+//     alert('Teste2')
+// }
+// aviso2()
+
+document.getElementById('titulo').innerHTML
